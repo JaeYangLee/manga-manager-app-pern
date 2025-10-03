9d32d1db5366e60e8780203af8569953021e0885
Jian Lee <ramosjianlee@gmail.com>
2025-10-03 16:40:45 +0800

    finished app.js

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const router = require("../backend/routes/mangaManagerRoutes");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/manga", router);
+
+module.exports = app;
