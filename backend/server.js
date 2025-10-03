c3a910b636dd277a514a4e058aa26317d46bdac9
Jian Lee <ramosjianlee@gmail.com>
2025-10-03 16:46:10 +0800

    finished server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const app = require("../backend/server");
 require("dotenv").config();
+const app = require("../backend/app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen((PORT) => {
-  console.log(`Server Running On http:://localhost/${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server Running On http://localhost:${PORT}`);
 });
