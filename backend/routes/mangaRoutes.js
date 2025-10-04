f93dd257bc1c0ca2257951a7a69a563ffe048a3c
Jian Lee <ramosjianlee@gmail.com>
2025-10-04 15:43:28 +0800

    fix: finished building routes, nested manga list routes to manga routes

--- backend/routes/mangaRoutes.js
+++ backend/routes/mangaRoutes.js
@@ -1,10 +1,27 @@
-const mangaManagerController = require("../controller/mangaController");
+const mangaController = require("../controller/mangaController");
+const mangaListController = require("../controller/mangaListController");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", mangaManagerController.getAllManga);
-router.post("/", mangaManagerController.addManga);
-router.put("/:manga_id", mangaManagerController.updateManga);
-router.delete("/:manga_id", mangaManagerController.deleteManga);
+router.get("/", mangaController.getAllManga);
+router.get("/:manga_id", mangaController.getMangaByID);
+router.post("/", mangaController.addManga);
+router.put("/:manga_id", mangaController.updateManga);
+router.delete("/:manga_id", mangaController.deleteManga);
+
+router.get("/:manga_id/list", mangaListController.getAllTrackedManga);
+router.get(
+  "/:manga_id/list/:manga_list_id",
+  mangaListController.getTrackedMangaByID
+);
+router.post("/:manga_id/list/", mangaListController.addTrackedManga);
+router.put(
+  "/:manga_id/list/:manga_list_id",
+  mangaListController.updateTrackedManga
+);
+router.delete(
+  "/:manga_id/list/:manga_list_id",
+  mangaListController.deleteTrackedManga
+);
 
 module.exports = router;
