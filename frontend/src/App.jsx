f3fb96712c6a278e8b173f7007542b3345ab46fb
Jian Lee <ramosjianlee@gmail.com>
2025-10-06 19:09:21 +0800

    fix: handling delete function to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./index.css";
 import axios from "axios";
+import MmMangaList from "./assets/MmMangaList";
 
 function App() {
   const [mangas, setMangas] = useState([]);
@@ -11,8 +12,8 @@ function App() {
 
   const getAllMangas = async () => {
     try {
-      const res = await axios.get("http://localhost:/5000/mangas");
-      setMangas(res);
+      const res = await axios.get("http://localhost:5000/mangas");
+      setMangas(res.data);
     } catch (err) {
       console.error("GET: Error Fetching All Mangas!", err.message);
     }
@@ -20,8 +21,8 @@ function App() {
 
   const getMangaById = async () => {
     try {
-      const res = await axios.get("http://localhost:/5000/mangas");
-      setMangas(res);
+      const res = await axios.get("http://localhost:5000/mangas");
+      setMangas(res.data);
     } catch (err) {
       console.error("GET: Error Fetching Manga By Id!", err.message);
     }
@@ -29,7 +30,7 @@ function App() {
 
   const addManga = async () => {
     try {
-      const res = await axios.post("http://localhost:/5000/mangas");
+      const res = await axios.post("http://localhost:5000/mangas");
       setMangas([...mangas, res.data]);
     } catch (err) {
       console.error("POST: Error Adding Manga!", err.message);
@@ -38,9 +39,9 @@ function App() {
 
   const updateManga = async () => {
     try {
-      const res = await axios.put(`http://localhost:/5000/mangas/${manga_id}`);
+      const res = await axios.put(`http://localhost:5000/mangas/${manga_id}`);
       setMangas(
-        mangas.map((manga) => (manga.manga_id === id ? res.data : manga))
+        mangas.map((manga) => (manga.manga_id === manga_id ? res.data : manga))
       );
     } catch (err) {
       console.error("PUT: Error Updating Manga!", err.message);
@@ -49,10 +50,8 @@ function App() {
 
   const deleteManga = async () => {
     try {
-      const res = await axios.delete(
-        `http://localhost:/5000/mangas/${manga_id}`
-      );
-      setMangas(mangas.filter((manga) => manga.manga_id !== manga.id));
+      await axios.delete(`http://localhost:5000/mangas/${manga_id}`);
+      setMangas(mangas.filter((mangas) => mangas.manga_id !== manga_id));
     } catch (err) {
       console.error("DELETE: Error Deleting Manga!", err.message);
     }
@@ -61,12 +60,16 @@ function App() {
   return (
     <>
       <div className="w-screen h-screen">
-        <header>
+        <header className="flex items-center justify-center font-bold text-2xl p-4">
           <h1>Manga Manager App</h1>
         </header>
 
         <main>
-          <h1>Catalog Page</h1>
+          <MmMangaList
+            mangas={mangas}
+            onUpdate={updateManga}
+            onDelete={deleteManga}
+          />
         </main>
       </div>
     </>
