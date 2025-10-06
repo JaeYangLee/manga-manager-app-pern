48348b2a9929571649c9d05a302c869af2df9554
Jian Lee <ramosjianlee@gmail.com>
2025-10-06 19:09:04 +0800

    fix: changes url request to "/mangas"

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use("/manga", router);
+app.use("/mangas", router);
 
 module.exports = app;
