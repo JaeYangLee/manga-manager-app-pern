81a212ac6e048464c4fd2683f913685ad31bef04
Jian Lee <ramosjianlee@gmail.com>
2025-10-13 13:44:44 +0800

    feat: built new page for manga tracker

---
+++ frontend/src/pages/MmMangaList.jsx
@@ -0,0 +1,26 @@
+import MmManga from "../assets/MmManga";
+
+function MmCatalogPage({ mangas = [], onUpdate, onDelete }) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <ul className="flex flex-col items-center justify-center gap-4">
+        {mangas.length === 0 ? (
+          <div className="flex flex-col items-center">
+            <p className="text-2xl font-bold">No Manga Found...</p>
+          </div>
+        ) : (
+          mangas.map((mangas) => (
+            <MmManga
+              key={mangas.manga_id}
+              manga={mangas}
+              onUpdate={onUpdate}
+              onDelete={() => onDelete(mangas.manga_id)}
+            />
+          ))
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default MmCatalogPage;
