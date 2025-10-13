f0a8791e12746a616be7873ac60ebc0ba38d5c7b
Jian Lee <ramosjianlee@gmail.com>
2025-10-14 05:42:06 +0800

    feat: implemented routes for uploading files

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const router = require("../backend/routes/mangaRoutes");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use("/mangas", router);
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // for uploading files
+app.use("/mangas", router); //main route
 
 module.exports = app;
