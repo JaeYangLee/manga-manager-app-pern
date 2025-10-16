7da7b62afde9cc43f349dad64e79c2d1f4f7587c
Jian Lee <ramosjianlee@gmail.com>
2025-10-16 10:32:39 +0800

    feat: initialized app.js to connect to server

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const mangaRoutes = require("./routes/mangaRoutes");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/manga", mangaRoutes);
+app.use("/uploads", express.static("uploads"));
+app.use((err, req, res, next) => {
+  console.error("[GLOBAL ERROR /app]:", err.message);
+  res.status(500).json({ error: "[GLOBAL ERROR /app]: Server error..." });
+});
+
+module.exports = app;
